7dcc1a559beeff4b079913d28e7fff3652dac9ed
Ayrton-Taede BeFoRe <ayrton.taede@gmail.com>
2020-03-18 23:56:49 +0100

    docker now ignores deploy scripts

--- deploy.bat
+++ deploy.bat
@@ -2,4 +2,4 @@ git pull
 docker build -t r6-operators .
 docker stop r6-operators 
 docker rm r6-operators
-docker run -p 8082:4444 -d --name r6-operators r6-operators
+docker run -p 8082:4444 -d --restart=unless-stopped --name r6-operators r6-operators
