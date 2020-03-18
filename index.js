195ab4a345622de230c8536264f2497758691805
Ayrton-Taede BeFoRe <ayrton.taede@gmail.com>
2020-03-18 16:02:48 +0100

    Initial release

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const helmet = require('helmet');
+const fs = require('fs');
+
+const app = express();
+const port = 4444;
+
+const operatorDataRaw = fs.readFileSync('./public/data/operators.json').toString();
+const operatorData = JSON.parse(operatorDataRaw);
+
+app.use(helmet());
+app.use(express.static('public'));
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  let data;
+  if (process.env.NODE_ENV === 'development') data = JSON.parse(fs.readFileSync('./public/data/operators.json').toString());
+  else data = operatorData;
+  res.render('index', { data: JSON.stringify(data), text: 'hallo' });
+});
+
+app.listen(port).on('listening', () => console.log(`Now listening on port ${port}`));
---
+++ public/js/index.js
@@ -0,0 +1,216 @@
+function operatorValue(operator) {
+  let value = 1;
+  operator.gadgets.forEach((gadget) => {
+    if (gadget === 'C4') value *= 1;
+    if (gadget === 'IMPACT') value *= 2;
+    if (gadget === 'SHIELD') value *= 3;
+    if (gadget === 'BARBED') value *= 4;
+    if (gadget === 'CAM') value *= 5;
+  });
+
+  return value;
+}
+
+function compareOperators(operator1, operator2) {
+  return operatorValue(operator1) - operatorValue(operator2);
+}
+
+const operatorsSorted = data.operators.sort(compareOperators);
+
+/**
+ * @typedef Hsl
+ * @property {number} h hue
+ * @property {number} s saturation
+ * @property {number} l luminosity
+ */
+
+function RGBToHSL(rgb) {
+  // Make r, g, and b fractions of 1
+  const r = rgb.r / 255;
+  const g = rgb.g / 255;
+  const b = rgb.b / 255;
+
+  // Find greatest and smallest channel values
+  const cmin = Math.min(r, g, b);
+  const cmax = Math.max(r, g, b);
+  const delta = cmax - cmin;
+  let h = 0;
+  let s = 0;
+  let l = 0;
+
+  // Calculate hue
+  // No difference
+  if (delta === 0) { h = 0; }
+  // Red is max
+  else if (cmax === r) h = ((g - b) / delta) % 6;
+  // Green is max
+  else if (cmax === g) h = (b - r) / delta + 2;
+  // Blue is max
+  else h = (r - g) / delta + 4;
+
+  h = Math.round(h * 60);
+
+  // Make negative hues positive behind 360°
+  if (h < 0) { h += 360; }
+
+  // Calculate lightness
+  l = (cmax + cmin) / 2;
+
+  // Calculate saturation
+  s = delta === 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+  // Multiply l and s by 100
+  s = +(s * 100).toFixed(1);
+  l = +(l * 100).toFixed(1);
+
+  return { h, s, l };
+}
+
+const removeHash = (hex) => (hex.charAt(0) === '#' ? hex.slice(1) : hex);
+
+const parseHex = (nakedHex) => {
+  const isShort = (
+    nakedHex.length === 3
+    || nakedHex.length === 4
+  );
+
+  const twoDigitHexR = isShort ? `${nakedHex.slice(0, 1)}${nakedHex.slice(0, 1)}` : nakedHex.slice(0, 2);
+  const twoDigitHexG = isShort ? `${nakedHex.slice(1, 2)}${nakedHex.slice(1, 2)}` : nakedHex.slice(2, 4);
+  const twoDigitHexB = isShort ? `${nakedHex.slice(2, 3)}${nakedHex.slice(2, 3)}` : nakedHex.slice(4, 6);
+  const twoDigitHexA = ((isShort ? `${nakedHex.slice(3, 4)}${nakedHex.slice(3, 4)}` : nakedHex.slice(6, 8)) || 'ff');
+
+  // const numericA = +((parseInt(a, 16) / 255).toFixed(2));
+
+  return {
+    r: twoDigitHexR,
+    g: twoDigitHexG,
+    b: twoDigitHexB,
+    a: twoDigitHexA,
+  };
+};
+
+const hexToDecimal = (hex) => parseInt(hex, 16);
+
+const hexesToDecimals = ({
+  r, g, b, a,
+}) => ({
+  r: hexToDecimal(r),
+  g: hexToDecimal(g),
+  b: hexToDecimal(b),
+  a: +((hexToDecimal(a) / 255).toFixed(2)),
+});
+
+/**
+ * @typedef Rgba
+ * @property {number} r
+ * @property {number} g
+ * @property {number} b
+ * @property {number} a
+ */
+
+/**
+ * Turns an old-fashioned css hex color value into a rgb color value.
+ *
+ * If you specify an alpha value, you'll get a rgba() value instead.
+ *
+ * @param hex The hex value to convert. ('123456'. '#123456', ''123', '#123')
+ * @return {Rgba} An rgb or rgba value. ('rgb(11, 22, 33)'. 'rgba(11, 22, 33, 0.5)')
+ */
+const hexToRgba = (hex) => {
+  const hashlessHex = removeHash(hex);
+  const hexObject = parseHex(hashlessHex);
+  const decimalObject = hexesToDecimals(hexObject);
+
+  return decimalObject;
+};
+
+/**
+ * Calculate relitive luminance
+ * @param {Rgba} color
+ * @returns {number} relitive luminance
+ */
+const relitiveLuminance = (color) => {
+  const Rs = color.r / 255;
+  const Gs = color.g / 255;
+  const Bs = color.b / 255;
+
+  let R;
+  if (Rs <= 0.03928) R = Rs / 12.92;
+  else R = ((Rs + 0.055) / 1.055) ** 2.4;
+
+  let G;
+  if (Gs <= 0.03928) G = Gs / 12.92;
+  else G = ((Gs + 0.055) / 1.055) ** 2.4;
+
+  let B;
+  if (Bs <= 0.03928) B = Bs / 12.92;
+  else B = ((Bs + 0.055) / 1.055) ** 2.4;
+
+  const L = 0.2126 * R + 0.7152 * G + 0.0722 * B;
+
+  return L;
+};
+
+/**
+ * Calculates the color contrast
+ * @param {Rgba} source
+ * @param {Rgba} destination
+ */
+const calculateContrast = (source, destination) => {
+  const finalR = source.a * (source.r - destination.r) + destination.r;
+  const finalG = source.a * (source.b - destination.g) + destination.g;
+  const finalB = source.a * (source.g - destination.b) + destination.b;
+
+  const finalL = relitiveLuminance({ r: finalR, g: finalG, b: finalB });
+  const sourceL = relitiveLuminance(destination);
+
+  const contrast = (Math.max(finalL, sourceL) + 0.05) / (Math.min(finalL, sourceL) + 0.05);
+
+  if (contrast < 1) return 1 / contrast;
+  return contrast;
+};
+
+/** @typedef {import('vue/types/vue')} Vue */
+
+/** @type {Vue} */
+const { Vue } = window;
+
+// eslint-disable-next-line no-unused-vars
+const app = new Vue({
+  el: '#vueApp',
+  data: {
+    operators: { ...operatorsSorted },
+  },
+  methods: {
+    filterOps(gadgetType) {
+      if (gadgetType) {
+        this.operators = operatorsSorted.filter((operator) => {
+          let hasGadget = false;
+          operator.gadgets.forEach((gadget) => {
+            if (gadget === gadgetType) hasGadget = true;
+          });
+          return hasGadget;
+        });
+      } else {
+        this.operators = { ...operatorsSorted };
+      }
+    },
+    bestRatio(hex) {
+      const destination = hexToRgba(hex);
+      const lightHex = '#FFFD';
+      const darkHex = '#000C';
+
+      const light = hexToRgba(lightHex);
+      const dark = hexToRgba(darkHex);
+
+      const lightContrast = calculateContrast(light, destination);
+      const darkContrast = calculateContrast(dark, destination);
+
+      console.log(`Light contrast: ${lightContrast}`);
+      console.log(`Dark contrast: ${darkContrast}`);
+
+      if (lightContrast > darkContrast) return lightHex;
+      return darkHex;
+    },
+  },
+});
