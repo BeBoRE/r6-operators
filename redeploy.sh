9a71bb893d326fba3133a2167ce48e2e2f91c571
Ayrton-Taede BeFoRe <ayrton.taede@gmail.com>
2020-03-18 21:31:59 +0100

    now restarts on system restart

--- redeploy.sh
+++ redeploy.sh
@@ -11,4 +11,4 @@ if [ "$(docker ps -a | grep r6-operators)" ]; then
 	echo removing r6-operators
 	docker rm r6-operators
 fi
-docker run -p 8082:4444 -d --name r6-operators r6-operators
+docker run -d -p 8082:4444 --restart=unless-stopped --name r6-operators r6-operators
